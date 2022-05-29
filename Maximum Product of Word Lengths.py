ef762e19e47368eb1c68347f31831951c0992123
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-28 21:16:13 -0700

    Add files via upload

--- Maximum Product of Word Lengths.py
+++ Maximum Product of Word Lengths.py
@@ -27,3 +27,12 @@ class Solution:
                 mask |= (1 << (ord(c) - ord('a')))
             d[mask] = max(d.get(mask, 0), len(w))
         return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
+
+        # Solution 3 hashset
+        char_set = [set(words[i]) for i in range(len(words))]
+        max_val = 0
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if not (char_set[i] & char_set[j]):
+                    max_val = max(max_val, len(words[i]) * len(words[j]))
+        return max_val
