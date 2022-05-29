27011c6f6cf6d7da2600c2ceaeabe21038dd873d
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-28 21:08:02 -0700

    Add files via upload

--- Maximum Product of Word Lengths.py
+++ Maximum Product of Word Lengths.py
@@ -18,3 +18,12 @@ class Solution:
                 if dont_share(i, j):
                     mx = max(mx, len(i) * len(j))
         return mx
+
+        # Solution 2
+        d = {}
+        for w in words:
+            mask = 0
+            for c in set(w):
+                mask |= (1 << (ord(c) - ord('a')))
+            d[mask] = max(d.get(mask, 0), len(w))
+        return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
